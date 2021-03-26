c81977ec0439523613c48eeb772619440a4c6cf7
17803909286 <1017762553@qq.com>
2021-03-26 10:11:48 +0800

    新增配置

---
+++ app/src/main/java/com/tanpengxfdl/ppjoke/JokeApplication.java
@@ -0,0 +1,10 @@
+package com.tanpengxfdl.ppjoke;
+
+import android.app.Application;
+
+public class JokeApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
